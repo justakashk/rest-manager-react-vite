25ab0f68b2989f110b3e0dd4ad9d7d1062fdaf7d
justakashk <akashkumar60907@gmail.com>
2025-12-07 13:30:16 +0530

    Deploying

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import Card from './Card.jsx'
+
+const App = () => {
+  return (
+   <div className="parent">
+
+      <Card/>
+
+   
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+
+const Card = () => {
+  return (
+      <div className="card">
+
+  <div>
+      <div className="top">
+      <img src="https://static.vecteezy.com/system/resources/previews/014/018/561/non_2x/amazon-logo-on-transparent-background-free-vector.jpg" alt="" />
+      <button> Save <Bookmark size = {12} /> </button>
+    </div>
+    <div className="center">
+    <h3>Amazon <span>5 days ago</span></h3>
+    <h2>Senior UI/UX Designer</h2>
+    <div className='tag'>
+      <h4>PartTime</h4>
+      <h4>SeniorLevel</h4>
+    </div>
+    </div>
+  </div>
+
+    <div className="bottom">
+      <div>
+        <h3>$120/hr</h3>
+        <p>Mumbai India</p>
+      </div>
+      <button>Apply Now</button>
+    </div>
+   </div>
+  )
+}
+
+export default Card
